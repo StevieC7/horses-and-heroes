b94cdca4792f562a57a251344fa901a63094385b
StevieC7 <stevenrcalhoun@outlook.com>
2022-08-08 09:04:30 -0500

    Add cards to card list

--- app.js
+++ app.js
@@ -22,6 +22,32 @@ GAME FLOW
 
 // Define constants
 // -card objects
+let cardList = [];
+class Card {
+    constructor(name, attack, health, description, art) {
+        this.name = name;
+        this.attack = attack;
+        this.health = health;
+        this.description = description;
+        this.art = art;
+    }
+    addToCardList () {
+        cardList.push(this);
+    }
+}
+const horse = new Card("Horse",1,1,"Just a regular horse.","https://placekitten.com/200/300");
+horse.addToCardList();
+const gregor = new Card("Gregor",2,2,"Slightly better than a horse.","https://placekitten.com/200/300");
+gregor.addToCardList();
+const aaron = new Card("Aaron",3,3,"It's pronounced A-A-ron!","https://placekitten.com/200/300");
+aaron.addToCardList();
+const stinker = new Card("Stinker",1,3,"He's a little stinker, ain't he?","https://placekitten.com/200/300");
+stinker.addToCardList();
+const sirstabsalot = new Card("Sir Stabsalot",5,2,"The third spear is a bit excessive.","https://placekitten.com/200/300");
+sirstabsalot.addToCardList();
+const stonewall = new Card("Stonewall",2,5,"Good at standing in the way.","https://placekitten.com/200/300");
+stonewall.addToCardList();
+console.log(cardList);
 
 
 // Define state variables w/o values (leave that for init function)
