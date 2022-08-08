1ae5d1ef53828be7e00e4578d92558ac5ab48857
StevieC7 <stevenrcalhoun@outlook.com>
2022-08-08 15:59:16 -0500

    Work through event propagation and passing the right objects from listeners into callback functions

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ let score;
 const turns = ["Player","CPU"];
 let turn;
 const phases = ["Setup","Draw","Play","Fight"];
-let currentPhase;
+let currentPhase = phases[0];
 let horseDeckCards;
 let heroDeckCards;
 let cpuDeckCards = ["TEST VALUE"];
@@ -81,55 +81,71 @@ const endTurnButton = document.querySelector("#end-turn-button");
 
 // Add functions called by event listeners (use arrow notation)
 const drawHorse = () => {
+    console.log("drawHorse fired.");
     playerHandCards.push(horse);
     currentPhase = phases[2];
 };
 const drawHero = () => {
+    console.log("drawHero fired.");
     playerHandCards.push(heroDeck[0]);
     heroDeckCards.shift();
     currentPhase = phases[2];
 };
 const inspectCard = (card) => {
+    console.log("inspectCard fired");
     inspectedCard = card;
 };
 const peekCard = (cardIndex) => {
+    console.log("peekCard fired.");
     peekedCard = cardIndex;
 };
 const selectCard = (card) => {
+    console.log("selectCard fired.");
     selectedCard = card;
 };
 const randomizeHeroDeck = () => {
     // randomize the player's hero deck and return an array of cards
     // TEST VALUE below
+    console.log("randomizeHeroDeck fired.");
     return [horse, gregor,sirstabsalot,gregor,horse];
-}
+};
 const randomizeCpuCards = () => {
     // randomize the CPU's cards and return an array of cards
     // TEST VALUE below
+    console.log("randomizeCpuCards fired.");
     return [horse, gregor,sirstabsalot,gregor,horse];
-}
+};
 const playerDrawCards = () => {
     // always draw one horse
     // randomly draw either horses or heroes for the rest of the cards
     // TEST VALUE below
+    console.log("playerDrawCards fired.");
+    currentPhase = phases[2];
     return [horse, gregor,sirstabsalot,gregor,horse];
-}
+};
 const cpuDrawCards = () => {
     // treat cpu the same way as player draw
     // TEST VALUE below
+    console.log("cpuDrawCards fired.");
+    currentPhase = phases[2];
     return [horse, gregor,sirstabsalot,gregor,horse];
-}
+};
 const resetGame = () => {
     // check if user is sure
-    prompt("Are you sure?");
+    prompt("Are you sure? Y/N");
     // if confirmation received, reset the game
     if (prompt.value === "Y") {
         init();
     };
-}
+};
 const endTurn = () => {
-    currentPhase = phases[3];
-}
+    console.log("End turn button fired.");
+    if(turn === turns[0] && currentPhase === phases[2]) {
+        currentPhase = phases[3];
+    } else if (turn === turn[0] && currentPhase === phases[1]) {
+        currentPhase = phases[3];
+    };
+};
 
 // Add event listeners
 // -cards in hand
@@ -140,6 +156,7 @@ const endTurn = () => {
 // -reset button
 // below event listener ensures the hand card the user is hovering over will peek out a bit
 playerHand.addEventListener("mouseover", event => {
+    console.log("playerHand mouseover fired.");
     for (let i = 0; playerHandCards.length; i++) {
         if (event.target !== playerHand.children[i]) {
             return;
@@ -148,17 +165,29 @@ playerHand.addEventListener("mouseover", event => {
     };
 });
 // below event listener ensures only the hand card the user is clicking on gets inspected
-playerHand.addEventListener("click", event => {
-    for (let i = 0; i < playerHandCards.length; i++) {
-        if (event.target !== playerHand.children[i]) {
-            return;
-        };
-        inspectCard(playerHandCards[i]);
-        selectCard(playerHandCards[i]);
+// playerHand.addEventListener("click", event => {
+//     for (let i = 0; i < playerHandCards.length; i++) {
+//         if (event.target !== playerHand.children[i]) {
+//             return;
+//         };
+//         inspectCard(playerHandCards[i]);
+//         selectCard(playerHandCards[i]);
+//     };
+// });
+const playerHandListener = () => {
+    console.log("playerHandListener click fired");
+    const playerHandChildren = playerHand.children;
+    for (let i = 0; i < playerHandChildren.length; i++) {
+        playerHandChildren[i].addEventListener("click", () => {
+            // event.stopPropagation;
+            inspectCard(playerHandCards[i]);
+            selectCard(playerHandCards[i]);
+        });
     };
-});
+};
 // below event listener ensures the card the user hovers over gets inspected
 cpuPlayArea.addEventListener("mouseover", event => {
+    console.log("cpuPlayArea mouseover fired.");
     for (let i = 0; i < cpuPlayArea.children.length; i++) {
         if (event.target !== cpuPlayArea.children[i]) {
             return;
@@ -168,6 +197,7 @@ cpuPlayArea.addEventListener("mouseover", event => {
 });
 // below event listener ensures the card the user hovers over on the player's board gets inspected
 playerPlayArea.addEventListener("mouseover", event => {
+    console.log("playerPlayArea mouseover fired.");
     for (let i = 0; i < playerPlayArea.children.length; i++) {
         if (event.target !== playerPlayArea.children[i]) {
             return;
@@ -176,7 +206,9 @@ playerPlayArea.addEventListener("mouseover", event => {
     };
 });
 // below event listener handles possibilities for the player's card slots
+// needs revisiting because it isn't working
 playerPlayArea.addEventListener("click", event => {
+    console.log(`playerPlayArea click fired with selected card "${selectedCard.name}".`);
     // if not user's turn, do nothing
     if (turn !== "Player") {
         return;
@@ -191,24 +223,28 @@ playerPlayArea.addEventListener("click", event => {
     };
     for (let i = 0; i < playerPlayAreaCards.length; i++) {
         if (event.target !== playerPlayArea.children[i]) {
-            return;
-        };
-        // if card slot is not empty and user has just clicked a horse card from hand, do nothing
-        if (playerPlayAreaCards[i] !== null && selectedCard.name === "Horse") {
-            return;
-        };
-        // if card slot has horse card and user has just clicked hero card from hand, play it and remove it from player hand
-        if (playerPlayAreaCards[i].name === "Horse" && selectedCard.name !== "Horse") {
-            playerPlayAreaCards.splice(i,1,selectedCard);
-            playerHandCards.splice(playerHandCards[i],1);
+            console.log("playerPlayArea event.target checker fired.");
             return;
         };
         // if card slot is empty and user has just clicked a horse card from hand, play it play it and remove it from player hand
         if (playerPlayAreaCards[i] === null && selectedCard.name === "Horse") {
+            console.log("playerPlayArea: slot is empty and you're playing a horse.");
             playerPlayAreaCards.splice(i,1,selectedCard);
             playerHandCards.splice(playerHandCards[i],1);
             return;
-        }
+        };
+        // if card slot is not empty and user has just clicked a horse card from hand, do nothing
+        if (playerPlayAreaCards[i] !== null && selectedCard.name === "Horse") {
+            console.log("playerPlayArea: slot not empty and you're trying to play Horse.");
+            return;
+        };
+        // if card slot has horse card and user has just clicked hero card from hand, play it and remove it from player hand
+        if (playerPlayAreaCards[i].name === "Horse" && selectedCard.name !== "Horse") {
+            console.log("playerPlayArea: slot has horse and you're playing a hero.");
+            playerPlayAreaCards.splice(i,1,selectedCard);
+            playerHandCards.splice(playerHandCards[i],1);
+            return;
+        };
         // if card slot is empty and user has just clicked hero card to play, alert player of illegal move
         if (playerPlayAreaCards[i] === null && selectedCard.name !== "Horse") {
             alert("You cannot play heroes directly to the board. You must first play a horse, then replace it with a hero.");
@@ -235,14 +271,14 @@ heroDeck.addEventListener("click", () => {
     drawHero();
 });
 // below functions set up start, reset, and end turn buttons
-startButton.addEventListener("click", init());
-resetButton.addEventListener("click", resetGame());
-endTurnButton.addEventListener("click", endTurn());
+startButton.addEventListener("click", () => {init()});
+resetButton.addEventListener("click", () => {resetGame()});
+endTurnButton.addEventListener("click", () => {endTurn()});
 
 // Invoke init function to initialize state variables
 function init() {
     score = 0;
-    turn = turn[0];
+    turn = turns[0];
     currentPhase = phases[1];
     horseDeckCards = [horse];
     heroDeckCards = randomizeHeroDeck();
@@ -253,9 +289,55 @@ function init() {
     cpuPlayAreaCards = [null, null, null];
     inspectedCard = null;
     selectedCard = null;
+    render();
 };
 
 // Invoke the main render function (transfer state variables to DOM)
+function render() {
+    console.log("render fired");
+    // update start, reset, and end turn buttons based on current phase
+    if (currentPhase !== phases[0]) {
+        startButton.style.display = "none";
+    } else if (currentPhase === phases[0]) {
+        startButton.style.display = "inline-block";
+        resetButton.style.display = "none";
+        endTurnButton.style.display = "none";
+    };
+    if (currentPhase === phases[2] || currentPhase === phases[1]) {
+        endTurnButton.style.display = "inline-block";
+    };
+    // update score meter with results from fight phase
+    // update card inspection to display currently inspected card
+    while (cardInspection.lastChild) {
+        console.log("Removing last inspected card");
+        cardInspection.removeChild(cardInspection.lastChild);
+    };
+    if (inspectedCard !== null && inspectedCard !== undefined) {
+        console.log("Inspected card element populated");
+        const inspectedCardElement = document.createElement("div");
+        inspectedCardElement.classList.add("inspected-card");
+        inspectedCardElement.innerHTML = `<img src=\"${inspectedCard.art}\"><p>${inspectedCard.description}</p><div class=\"attack-power\">${inspectedCard.attack}</div><div class=\"card-health\">${inspectedCard.health}</div>`
+        cardInspection.append(inspectedCardElement);
+    }
+    // update player hand
+    while (playerHand.lastChild) {
+        playerHand.removeChild(playerHand.lastChild);
+    };
+    playerHandCards.forEach((val) => {
+        const playerHandCardElement = document.createElement("div");
+        playerHandCardElement.classList.add("player-hand-card");
+        playerHandCardElement.innerHTML = `<img src=\"${val.art}\"><p>${val.description}</p><div class=\"attack-power\">${val.attack}</div><div class=\"card-health\">${val.health}</div>`
+        playerHand.append(playerHandCardElement);
+    });
+    playerHandListener();
+    // update play area
+    // update cpu hand
+    // update game log
+    // update horse deck
+    // update hero deck
+}
+// setInterval(() => render(),500);
+// render();
 
 // Wait for user to trigger event (loop/timer ?)
 
