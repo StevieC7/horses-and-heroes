ccf8d33c5b0f56d3ac9552081eb1fff80760c3cb
StevieC7 <stevenrcalhoun@outlook.com>
2022-08-11 09:01:48 -0500

    Fix inspectCard listener and remove comments

--- app.js
+++ app.js
@@ -1,25 +1,3 @@
-/* 
-WIN CONDITION
--If the score meter maxes out at 5 on a player's side, that player loses.
-GAME FLOW
-0. Decks shuffled and set for play
-1. Players draw their hands
-2. Play phase
-    -(expect first turn) Player draws either a horse or a hero card. The horse deck is an unlimited supply.
-    -Player may play as many cards in one turn as there are available spots on their side of the board.
-    -Player may play a horse into an empty spot. Horses are weak, but necessary to play heroes.
-    -Player may play a hero onto a horse, but not onto an empty spot. If played onto horse, the mounted horse is removed from play.
-3. Fight phase
-    -Player's cards are compared against CPU's, one by one
-    -The cards' attack powers reduce their opposing card's health
-    -If a card loses all health, it is discarded
-    -If a card opposes an empty spot, its attack power is applied against the player/cpu score meter
-    -If a card's attack causes either player's score to cross the "loss" threshold, that player loses.
-4. Repeat play phase and fight phase until a player loses.
-*/
-
-// Define constants
-// -card objects
 let cardList = [];
 class Card {
     constructor(name, attack, health, description, art) {
@@ -46,7 +24,6 @@ sirstabsalot.addToCardList();
 const stonewall = new Card("Stonewall",2,5,"Good at standing in the way.","https://placekitten.com/200/300");
 stonewall.addToCardList();
 
-// Define state variables w/o values (leave that for init function)
 let score;
 const turns = ["Player","CPU"];
 let turn;
@@ -64,7 +41,6 @@ let peekedCard;
 let selectedCard;
 let gameLogQueue = [null];
 
-// Select HTML elements that will be used more than once
 const playArea = document.querySelector("#play-area");
 const cpuPlayArea = document.querySelector("#cpu-play-area");
 const playerPlayArea = document.querySelector("#player-play-area");
@@ -79,10 +55,7 @@ const startButton = document.querySelector("#start-button");
 const resetButton = document.querySelector("#reset-button");
 const endTurnButton = document.querySelector("#end-turn-button");
 const endGameButton = document.querySelector("#end-game-button");
-// const renderButton = document.querySelector("#render-button");
 
-// Add functions called by event listeners (use arrow notation)
-//add clone function to call when needed for generating new cards in draw functions below
 const cloneCards = (original) => {
     const clonedCards = structuredClone(original);
     return clonedCards;
@@ -122,8 +95,6 @@ const randomizeHorseDeck = () => {
     return horseDeckClone;
 }
 const randomizeHeroDeck = () => {
-    // randomize the player's hero deck and return an array of cards
-    // TEST VALUE below
     let heroDeckClone = [];
     cardList.forEach((val) => {
         if (val.name !== "Horse") {
@@ -133,16 +104,11 @@ const randomizeHeroDeck = () => {
     return heroDeckClone;
 };
 const randomizeCpuCards = () => {
-    // randomize the CPU's cards and return an array of cards
-    // TEST VALUE below
     let cpuDeckClone = [];
     cardList.forEach((val) => {cpuDeckClone.push(cloneCards(val))});
     return cpuDeckClone;
 };
 const playerDrawCards = () => {
-    // always draw one horse
-    // randomly draw either horses or heroes for the rest of the cards
-    // TEST VALUE below
     let tempPlayerDraw = [];
     tempPlayerDraw.push(cloneCards(horseDeckCards[0]));
     tempPlayerDraw.push(cloneCards(heroDeckCards[0]),cloneCards(heroDeckCards[1]),cloneCards(heroDeckCards[2]));
@@ -152,8 +118,6 @@ const playerDrawCards = () => {
     return tempPlayerDraw;
 };
 const cpuFirstDrawCards = () => {
-    // treat cpu the same way as player draw
-    // TEST VALUE below
     let tempCpuDraw = [];
     for (let i = 0; i < cpuDeckCards.length; i++) {
         tempCpuDraw.push(cpuDeckCards[i]);
@@ -162,7 +126,6 @@ const cpuFirstDrawCards = () => {
     return tempCpuDraw;
 };
 const cpuDrawCards = () => {
-    // write function to have cpu draw from its own cloned decks like a human player would
     let cpuHorseDeck = [];
     cpuHorseDeck.push(cloneCards(horse));
     let cpuHeroDeck = [];
@@ -176,8 +139,6 @@ const cpuDrawCards = () => {
     };
 };
 const resetGame = () => {
-    // check if user is sure
-    // if confirmation received, reset the game
     location.reload();
 };
 const endTurn = () => {
@@ -186,44 +147,17 @@ const endTurn = () => {
         logEvent(`Now it's time to ${currentPhase}`);
         turn = turns[1];
         logEvent(`Turn: ${turn}`);
-        cpuPlayAreaListener();
+        cpuAI();
         return;
     } else if (turn === turns[0] && currentPhase === phases[1]) {
         currentPhase = phases[3];
         logEvent(`Now it's time to ${currentPhase}`);
         turn = turns[1];
         logEvent(`Turn: ${turn}`);
-        cpuPlayAreaListener();
+        cpuAI();
         return;
     };
 };
-
-// Add event listeners
-// -cards in hand
-// -play area
-// -decks
-// -card inspector
-// -start button
-// -reset button
-// below event listener ensures the hand card the user is hovering over will peek out a bit
-// playerHand.addEventListener("mouseover", event => {
-//     for (let i = 0; playerHandCards.length; i++) {
-//         if (event.target !== playerHand.children[i]) {
-//             return;
-//         };
-//         peekCard(i);
-//     };
-// });
-// below event listener ensures only the hand card the user is clicking on gets inspected
-// playerHand.addEventListener("click", event => {
-//     for (let i = 0; i < playerHandCards.length; i++) {
-//         if (event.target !== playerHand.children[i]) {
-//             return;
-//         };
-//         inspectCard(playerHandCards[i]);
-//         selectCard(playerHandCards[i]);
-//     };
-// });
 const playerHandListener = () => {
     const playerHandChildren = playerHand.children;
     for (let i = 0; i < playerHandChildren.length; i++) {
@@ -236,7 +170,14 @@ const playerHandListener = () => {
         // });
     };
 };
-// below event listener ensures the card the user hovers over gets inspected
+const cpuPlayAreaListener = () => {
+    const cpuPlayAreaChildren = cpuPlayArea.children;
+    for (let i = 0; i < cpuPlayAreaChildren.length; i++) {
+        cpuPlayAreaChildren[i].addEventListener("click", () => {
+            inspectCard(cpuPlayAreaCards[i]);
+        })
+    }
+}
 cpuPlayArea.addEventListener("mouseover", event => {
     for (let i = 0; i < cpuPlayArea.children.length; i++) {
         if (event.target !== cpuPlayArea.children[i]) {
@@ -245,100 +186,70 @@ cpuPlayArea.addEventListener("mouseover", event => {
         inspectCard(cpuPlayAreaCards[i]);
     };
 });
-// below is callback function for updating the cpu play area model during render cycle
-const cpuPlayAreaListener = () => {
-    console.log("cpuPlayAreaListener is running");
-    // for (let i = 0; i < 3; i++) {
-        if (turn !== turns[1]) {
-            return;
-        }
-        if (currentPhase !== "Fight") {
-            return;
-        }
-        console.log("AI logic running.")
-        // cpuHandCards.forEach((val) => console.log(val));
-        cpuDrawCards();
-        console.log("CPU drew a card. CPU now has " + cpuHandCards.length + " cards.");
-        // TODO - write new AI for cpu below
-        //      first loop over hand to see if there are horses and where they are
-        let cpuHandHorses = [];
-        for (let i = 0; i < cpuHandCards.length; i++) {
-            if (cpuHandCards[i].name === "Horse") {
-                cpuHandHorses.push(i);
-            };
+const cpuAI = () => {
+    console.log("cpuAI is running");
+    if (turn !== turns[1]) {
+        return;
+    }
+    if (currentPhase !== "Fight") {
+        return;
+    }
+    console.log("AI logic running.")
+    cpuDrawCards();
+    console.log("CPU drew a card. CPU now has " + cpuHandCards.length + " cards.");
+    let cpuHandHorses = [];
+    for (let i = 0; i < cpuHandCards.length; i++) {
+        if (cpuHandCards[i].name === "Horse") {
+            cpuHandHorses.push(i);
         };
-        // cpuHandCards.forEach((val) => console.log(val));
-        console.log(cpuHandHorses);
-        //      then if there are horses, check to see if there are open slots and where they are
-        let openSlots = [];
-        if (cpuHandHorses.length >= 1) {
-            for (let i = 0; i < cpuPlayAreaCards.length;i++) {
-                if (cpuPlayAreaCards[i] === null) {
-                    openSlots.push(i);
-                };
-            };
-        };
-        console.log(openSlots);
-        //      if there are open slots and horses, play horses until you are out of horses
-        let horsePlayIterator = 0;
-        while (horsePlayIterator < cpuHandHorses.length && horsePlayIterator < openSlots.length) {
-            console.log("horseplay while loop running on cpuPlayArea " + openSlots[horsePlayIterator] + " and inserting " + cpuHandCards[cpuHandHorses[horsePlayIterator]]);
-            cpuPlayAreaCards.splice(openSlots[horsePlayIterator],1,cloneCards(cpuHandCards[cpuHandHorses[horsePlayIterator]]));
-            console.log("CPU playing " + cpuHandCards[cpuHandHorses[horsePlayIterator]].name);
-            // cpuHandHorses.shift();
-            horsePlayIterator++;
-        };
-        for (let i = 0; i < cpuHandHorses.length; i++) {
-            console.log("Removing horse at " + cpuHandHorses[i] + " from hand.");
-            cpuHandCards.splice(cpuHandHorses[i],1);
-        };
-        //      next, loop over hand to see if there are any cards left
-        let cpuHandHeroes = [];
-        for (let i = 0; i < cpuHandCards.length; i++) {
-            cpuHandHeroes.push(i);
-            console.log("CPU found hero at " + cpuHandHeroes[i]);
-        };
-        // console.log(cpuHandHeroes);
-        //      if there are cards left, pick the one with lowest total power (easy AI)
-        //          -sort array of cpuHandHeroes by attack power
-        cpuHandHeroes.sort((a,b) => a.attack - b.attack);
-        // console.log(cpuHandHeroes);
-        //      loop over spots to see if there are any with horses
-        for (let i = 0; i < cpuPlayAreaCards.length;i++) {
-            console.log("CPU looking for horse to play hero on");
-            if (cpuPlayAreaCards[i] !== null && cpuPlayAreaCards[i].name === "Horse") {
-                cpuPlayAreaCards.splice(i,1,cloneCards(cpuHandCards[i]));
-                console.log("CPU hand cards before playing" + cpuHandCards);
-                console.log("CPU played " + cpuHandCards[i].name);
-                cpuHandCards.splice(cpuHandCards.indexOf(cpuHandHeroes[i]),1);
-                console.log("CPU hand cards after playing" + cpuHandCards);
-                console.log("CPU removed card " + cpuHandCards[cpuHandHeroes[i]].name + " at " + cpuHandHeroes[i]);
-            };
-        };
-        console.log("CPU now has " + cpuHandCards.length + " cards.");
-        //      if horse in spot, play hero card
-
-        // TODO - remove this Original "AI" for MVP once the new AI is written
-        // if (cpuHandCards.length >= 1) {
-        //     cpuPlayAreaCards.splice(0,1,cpuHandCards[0]);
-        //     console.log("CPU playing card: " + cpuPlayAreaCards[0]);
-        //     cpuHandCards.splice(0,1);
-        // };
-    // }
-};
-// below event listener ensures the card the user hovers over on the player's board gets inspected
-playerPlayArea.addEventListener("mouseover", event => {
-    for (let i = 0; i < playerPlayArea.children.length; i++) {
-        if (event.target !== playerPlayArea.children[i]) {
-            return;
-        };
-        inspectCard(playerPlayAreaCards[i]);
     };
-});
-// new event listener for playerPlayArea
+    console.log(cpuHandHorses);
+    let openSlots = [];
+    if (cpuHandHorses.length >= 1) {
+        for (let i = 0; i < cpuPlayAreaCards.length;i++) {
+            if (cpuPlayAreaCards[i] === null) {
+                openSlots.push(i);
+            };
+        };
+    };
+    console.log(openSlots);
+    let horsePlayIterator = 0;
+    while (horsePlayIterator < cpuHandHorses.length && horsePlayIterator < openSlots.length) {
+        console.log("horseplay while loop running on cpuPlayArea " + openSlots[horsePlayIterator] + " and inserting " + cpuHandCards[cpuHandHorses[horsePlayIterator]]);
+        cpuPlayAreaCards.splice(openSlots[horsePlayIterator],1,cloneCards(cpuHandCards[cpuHandHorses[horsePlayIterator]]));
+        console.log("CPU playing " + cpuHandCards[cpuHandHorses[horsePlayIterator]].name);
+        horsePlayIterator++;
+    };
+    for (let i = 0; i < cpuHandHorses.length; i++) {
+        console.log("Removing horse at " + cpuHandHorses[i] + " from hand.");
+        cpuHandCards.splice(cpuHandHorses[i],1);
+    };
+    let cpuHandHeroes = [];
+    for (let i = 0; i < cpuHandCards.length; i++) {
+        cpuHandHeroes.push(i);
+        console.log("CPU found hero at " + cpuHandHeroes[i]);
+    };
+    cpuHandHeroes.sort((a,b) => a.attack - b.attack);
+    for (let i = 0; i < cpuPlayAreaCards.length;i++) {
+        console.log("CPU looking for horse to play hero on");
+        if (cpuPlayAreaCards[i] !== null && cpuPlayAreaCards[i].name === "Horse") {
+            cpuPlayAreaCards.splice(i,1,cloneCards(cpuHandCards[i]));
+            console.log("CPU hand cards before playing" + cpuHandCards);
+            console.log("CPU played " + cpuHandCards[i].name);
+            cpuHandCards.splice(cpuHandCards.indexOf(cpuHandHeroes[i]),1);
+            console.log("CPU hand cards after playing" + cpuHandCards);
+            console.log("CPU removed card " + cpuHandCards[cpuHandHeroes[i]].name + " at " + cpuHandHeroes[i]);
+        };
+    };
+    console.log("CPU now has " + cpuHandCards.length + " cards.");
+};
 const playerPlayAreaListener = () => {
-    for (let i = 0; i < playerPlayArea.children.length; i++) {
-        playerPlayArea.children[i].addEventListener("click", (e) => {
+    const playerPlayAreaChildren = playerPlayArea.children;
+    for (let i = 0; i < playerPlayAreaChildren.length; i++) {
+        playerPlayAreaChildren[i].addEventListener("mouseover", () => {
+            inspectCard(playerPlayAreaCards[i]);
+        });
+        playerPlayAreaChildren[i].addEventListener("click", (e) => {
             e.stopImmediatePropagation();
             // if not user's turn, do nothing
             if (turn !== "Player") {
@@ -381,7 +292,6 @@ const playerPlayAreaListener = () => {
     }
     return;
 };
-// below events ensure user can draw during draw phase
 horseDeck.addEventListener("click", () => {
     if (turn !== "Player") {
         return;
@@ -400,14 +310,11 @@ heroDeck.addEventListener("click", () => {
     };
     return drawHero();
 });
-// below functions set up start, reset, and end turn buttons
 startButton.addEventListener("click", () => {init()});
 resetButton.addEventListener("click", () => {resetGame()});
 endTurnButton.addEventListener("click", () => {endTurn()});
 endGameButton.addEventListener("click", () => {endGame()});
-// renderButton.addEventListener("click", () => {render()});
 
-// Invoke init function to initialize state variables
 function init() {
     score = 0;
     turn = turns[0];
@@ -421,7 +328,6 @@ function init() {
     cpuPlayAreaCards = [null, null, null];
     inspectedCard = null;
     selectedCard = null;
-    // cpuPlayAreaListener();
     render();
 };
 const logEvent = (message) => {
@@ -432,6 +338,7 @@ const logEvent = (message) => {
     gameLogQueue.push(gameLogDisplay);
     return;
 }
+
 function fight() {
     if (currentPhase !== "Fight") {
         return;
@@ -464,19 +371,11 @@ function fight() {
     logEvent(`Turn: ${turn}`);
     currentPhase = "Draw";
     logEvent(`Now it's time to ${currentPhase}`);
-    // let postFightRenderDelay = setTimeout(() => render(),5000);
-    // function stopTimer() {
-    //     clearTimeout(postFightRenderDelay);
-    //     postFightRenderDelay = null;
-    // }
-    // stopTimer();
     render();
     return;
 };
 
-// Invoke the main render function (transfer state variables to DOM)
 function render() {
-    // update start, reset, and end turn buttons based on current phase
     if (currentPhase !== phases[0]) {
         startButton.style.display = "none";
     } else if (currentPhase === phases[0]) {
@@ -565,11 +464,10 @@ function render() {
     for (let i = 0; i < cpuPlayAreaCardElements.length; i++) {
         cpuPlayArea.children[i].replaceWith(cpuPlayAreaCardElements[i]);
     };
-    // update game log
+    cpuPlayAreaListener();
     // STRETCH: update hero deck graphics
     // STRETCH: update cpu hand graphics
     if (currentPhase === "Fight") {
-        // cpuPlayAreaListener();
         const fightDelay = (ms) => new Promise(resolve => setTimeout(resolve,ms));
         fightDelay(3000).then(() => fight());
     };
@@ -619,8 +517,3 @@ function endGame() {
     clearInterval(renderCycle);
     renderCycle = null;
 }
-    // Wait for user to trigger event (loop/timer ?)
-    
-    // Update states based on user action
-
-// Invoke render function again
